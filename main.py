daeb6a4bed0d8f863e63f971e010e7d978ba7049
stevisco <stefano.visconti@gmail.com>
2022-03-16 21:01:15 +0100

    insert with params

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from threading import Thread
-from flask import Flask, abort, jsonify
-from poller import poller_task,insert_instantmeeting
+from flask import Flask, abort, jsonify, request
+from RoomStatus import RoomStatus
+from poller import get_calendar_status, poller_task,insert_instantmeeting
 from datetime import datetime,timezone,timedelta 
 import time 
 
@@ -13,16 +14,28 @@ def index():
     return "OK"
 
 
-@app.route("/newmeeting")
+@app.route("/newmeeting",methods=['POST'])
 def newmeeting():
-    calendar_id='arduino.cc_3931313135363730363336@resource.calendar.google.com'
-    room_name="blue_room"
-    client_secret="***REMOVED***"
-    client_id="SOeu9scvEKBMrRjG8olnDAwegufvTiCp"
+    content = request.get_json(True)
+    calendar_id = content.get("calendar_id","")
+    room_name = content.get("room_name","")
+    client_secret = content.get("client_secret","")
+    client_id = content.get("client_id","")
+    duration_str = content.get("duration_mins","60")
+    if calendar_id=="" or room_name=="" or client_secret=="" or client_id=="":
+        abort(400,"Required parameters in request body are missing")
+    #calendar_id='arduino.cc_3931313135363730363336@resource.calendar.google.com'
+    #room_name="blue_room"
+    #client_secret="***REMOVED***"
+    #client_id="SOeu9scvEKBMrRjG8olnDAwegufvTiCp"
 
-    duration_mins = 30
-    code = insert_instantmeeting(calendar_id,room_name,duration_mins)
-    if code: 
+    duration_mins = int(duration_str)
+    roomstatus = get_calendar_status(calendar_id,room_name)
+    if (roomstatus.busynow==RoomStatus.BUSY):
+        abort(400,"Could not insert new meeting, room already busy")
+
+    insertedok = insert_instantmeeting(calendar_id,room_name,duration_mins)
+    if insertedok: 
         return "Meeting inserted"
     else: 
         abort(500,"ERROR - could not insert meeting")
